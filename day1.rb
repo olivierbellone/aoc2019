bfd2f6054195b9ab3ea035a843a2fbd11e2d5c3b
Olivier Bellone <olivier@bellone.fr>
2019-12-02 20:51:32 -0800

    Day 2

--- day1.rb
+++ day1.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-puts File.read('day1.txt').split.map { |m| (m.to_i / 3).floor - 2 }.sum
+input = File.read('day1.txt').split.map(&:to_i)
+
+# Part 1
+
+puts input.map { |m| (m / 3).floor - 2 }.sum
+
+# Part 2
+
+def fuel(mass)
+  f = (mass / 3).floor - 2
+  return 0 if f <= 0
+
+  f + fuel(f)
+end
+
+puts input.map { |m| fuel(m) }.sum
--- day2.rb
+++ day2.rb
@@ -1,11 +1,44 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def fuel(mass)
-  f = (mass / 3).floor - 2
-  return 0 if f <= 0
+input = File.read('day2.txt').split(',').map(&:to_i)
 
-  f + fuel(f)
+def process(program, pos=0)
+  opcode = program[pos]
+  pos1, pos2, pos3 = program[pos + 1..pos + 3]
+  case opcode
+  when 1
+    program[pos3] = program[pos1] + program[pos2]
+  when 2
+    program[pos3] = program[pos1] * program[pos2]
+  when 99
+    return program[0]
+  else
+    puts "Unknown opcode #{opcode} at position #{pos}"
+  end
+  process(program, pos + 4)
 end
 
-puts File.read('day1.txt').split.map { |m| fuel(m.to_i) }.sum
+# Part 1
+
+program = input.dup
+
+program[1] = 12
+program[2] = 2
+
+p process(program)
+
+# Part 2
+
+(0..99).each do |noun|
+  (0..99).each do |verb|
+    program = input.dup
+    program[1] = noun
+    program[2] = verb
+    result = process(program)
+    if result == 19_690_720
+      p 100 * noun + verb
+      exit(0)
+    end
+  end
+end
